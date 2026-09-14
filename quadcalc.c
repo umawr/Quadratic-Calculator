00ffe2d982744e9efb848b67ae6357e768f88a69
Umair <umaircapalot@gmail.com>
2026-09-14 18:13:20 +0530

    Update quadcalc.c

--- quadcalc.c
+++ quadcalc.c
@@ -15,20 +15,34 @@ int main(void)
     break;
 
     case 'c':
-    // printf("Yos choose coeffs!\n");
-    printf("Enter a(x squared) b(x) c\n");
+    printf("Enter a(x squared) b(x) c\n\n");
     scanf("%f %f %f", &a, &b, &c);
     d = b*b - 4*a*c;
-    if (d >= 0)
+    printf("%-16s: %.2f\n", "x squared coeff", a);
+    printf("%-16s: %.2f\n", "x coeff", b);
+    printf("%-16s: %.2f\n", "constant", c);    if (d >= 0)
     {
-      printf("Roots are real i.e, dicriminant is postive\n");
-
+      
       r1 = (-b+sqrt(d))/(2*a);
       r2 = (-b-sqrt(d))/(2*a);
+      
+      printf("\nDiscriminant is %.2f\n", d); 
+      if (r1 > r2)
+      {
+        printf("\nRoots are real, dicriminant is postive\n\n");
+        printf("1st root is %f\n", r1);
+        printf("2nd root is %f\n\n", r2);
+      } else if (r2 > r1)
+      {
+        printf("\nRoots are real, dicriminant is postive\n\n");
+        printf("1st root is %f\n", r2);
+        printf("2nd root is %f\n\n", r1);
 
-      printf("Discriminat is %f\n", d);
-      printf("1st root is %f\n", r1);
-      printf("2nd root is %f\n", r2);
+      } else if (r1 = r2)
+      {
+        printf("\nRoots are equal, dicriminant is zero\n\n");
+        printf("Both roots are %f\n\n", r1);
+      }
     } else if(d < 0)
     {
       printf("have to fix");
