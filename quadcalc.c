136e195b4f59cf282997770bbfd5f7a7faf1ee1a
Umair <umaircapalot@gmail.com>
2026-09-14 16:06:46 +0530

    Update quadcalc.c

--- quadcalc.c
+++ quadcalc.c
@@ -2,10 +2,9 @@
 #include<math.h>
 #include<complex.h>
 
-int main()
+int main(void)
 {
-  float a, b, c, d, r1, r2;
-  printf("hellppppppp");
-  return 0;
-  
+  float a, b, c, d, r1, r2, o1, o2;
+  printf("Roots(r) or coefficients(c)?");
+  scanf("%c %c", &o1, &o2)
 }
