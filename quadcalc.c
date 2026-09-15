811e66a203ee070d5a5d7e1b5f48e8bd4e972700
Umair <umaircapalot@gmail.com>
2026-09-15 21:58:30 +0530

    Update quadcalc.c

--- quadcalc.c
+++ quadcalc.c
@@ -1,26 +1,50 @@
 #include<stdio.h>
 #include<math.h>
-#include<complex.h>
 
 int main(void)
 {
-  float a, b, c, d, r1, r2, real, imaginary;
+  float a, b, c, d, sor, por, r1, r2, real, imaginary;
   char o;
   printf("Roots(r) or coefficients(c)?\n");
   scanf("%c", &o);
   switch (o) 
   {
     case 'r':
+    
     printf("You choose roots!\n");
+    printf("Enter the roots\n");
+    scanf("%f %f", &r1, &r2);
+    if (r1 > r2)
+    {
+      printf("1st root is %.2f\n", r1);
+      printf("2nd root is %.2f\n", r2);
+    } else if (r2 > r1)
+    {
+      printf("1st root is %.2f\n", r2);
+      printf("2nd root is %.2f\n", r1);
+
+    } else
+    {
+      printf("Both roots are %.2f\n", r1);
+
+    }
+    sor = r1 + r2;
+    por = r1*r2;
+
+    printf("\nx squared %+.2fx %+.2f\n", sor, por);
+    printf("\nSum of roots are %.2f\n", sor);
+    printf("Product of roots are %.2f\n\n", por);
+    
     break;
 
     case 'c':
     printf("Enter a(x squared) b(x) c\n\n");
     scanf("%f %f %f", &a, &b, &c);
     d = b*b - 4*a*c;
-    printf("%-16s: %f\n", "x squared coeff", a);
+    printf("\n%-16s: %f\n", "x squared coeff", a);
     printf("%-16s: %f\n", "x coeff", b);
-    printf("%-16s: %f\n", "constant", c);    if (d >= 0)
+    printf("%-16s: %f\n", "constant", c);    
+    if (d >= 0)
     {
       
       r1 = (-b+sqrt(d))/(2*a);
@@ -46,7 +70,7 @@ int main(void)
     } else if(d < 0)
     {
       real = -b/(2*a);
-      imaginary = sqrt(-d)/(2*a);
+      imaginary = sqrt(-d)/(2*a); // '-'d to make the under root number positive
       printf("\nRoots are complex\n\n");
       printf("1st root is  %.2f + %.2fi\n", real, imaginary);
       printf("2nd root is  %.2f - %.2fi\n\n", real, imaginary);
